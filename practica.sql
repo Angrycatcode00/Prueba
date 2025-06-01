47b483415c7be319a7fe111b34da910e1ebb4723
Angrycatcode00 <s.alejandramm555@gmail.com>
2025-05-31 23:54:37 -0400

    Agrega script SQL para productos
    holas

---
+++ practica.sql
@@ -0,0 +1,15 @@
+-- Crear tabla
+CREATE TABLE productos (
+    id SERIAL PRIMARY KEY,
+    nombre TEXT NOT NULL,
+    precio NUMERIC(10, 2) NOT NULL
+
+);
+-- Insertar datos
+INSERT INTO productos (nombre, precio) VALUES
+('Pelota', 3500),
+('Cuaderno', 1200),
+('Botella', 2400);
+
+-- Consultar
+SELECT * FROM productos;
